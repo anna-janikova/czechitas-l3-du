aaa74934248209598ac022f7d274ec80539d4579
Anna Janíková <anna.janikova42@gmail.com>
2023-02-08 21:07:19 +0100

    DÚ změny

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // tady je místo pro náš program
 
+let celeJmeno = prompt("Jaké je Vaše jméno a příjmení?");
+let vek = Number(prompt("Kolik je Vám let"))
 
+let vysledek = document.querySelector(".vysledek")
 
+vysledek.innerHTML = "Jmenuji se " + celeJmeno +" a je mi " + vek + " let."
 
+let oblibenaBarva = prompt("What is your favourite color?")
+
+vysledek.style.color = oblibenaBarva
